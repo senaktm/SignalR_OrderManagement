213869e178680cbeb3d5a39b38f7dafb7640e6e7
senaktm <senaoktm@gmail.com>
2024-05-06 00:19:04 +0300

    migration added

---
+++ SignalR.DataAccessLayer/Migrations/20240505211316_initialize.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    AboutID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AboutImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AboutDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.AboutID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryStatus = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FooterLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FooterPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FootertEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FooterDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    DiscountID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiscountTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountAmount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.DiscountID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    FeatureID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FeatureTitle1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FeatureDescription1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FeatureTitle2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FeatureDescription2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FeatureTitle3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FeatureDescription3 = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.FeatureID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductStatus = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    ReservationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReservationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReservationPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReservationEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReservationPersonCount = table.Column<int>(type: "int", nullable: false),
+                    ReservationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.ReservationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMedias",
+                columns: table => new
+                {
+                    SocialMediaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SocialMediaTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SocialMediaIcon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SocialMediaUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMedias", x => x.SocialMediaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Testimonials",
+                columns: table => new
+                {
+                    TestimonialID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestimonialName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestimonialTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestimonialComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestimonialImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TestimonialStatus = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testimonials", x => x.TestimonialID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Discounts");
+
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "SocialMedias");
+
+            migrationBuilder.DropTable(
+                name: "Testimonials");
+        }
+    }
+}
