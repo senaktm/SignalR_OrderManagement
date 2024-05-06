d32b7b997fc108337a4aca95b87227ba1d8bae2b
senaktm <senaoktm@gmail.com>
2024-05-06 13:35:36 +0300

    EntityFramework for entities created

---
+++ SignalR.DataAccessLayer/EntityFramework/EfTestimonialDal.cs
@@ -0,0 +1,19 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repository;
+using SignalR.EntityLayer.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfTestimonialDal : GenericRepository<Testimonial>, ITestimonialDal
+    {
+        public EfTestimonialDal(Context context) : base(context)
+        {
+        }
+    }
+}
