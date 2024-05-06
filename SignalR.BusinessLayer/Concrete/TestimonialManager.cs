efccb006ea69e2d84dd8c708fa85787651f2f8a7
senaktm <senaoktm@gmail.com>
2024-05-06 15:39:20 +0300

    Manager classes created

---
+++ SignalR.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,38 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.EntityLayer.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        public void TAdd(Testimonial entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Testimonial> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
