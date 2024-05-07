6f257de7118bbd9ac7d36bbe587a39fdef4f0a19
senaktm <senaoktm@gmail.com>
2024-05-07 14:17:06 +0300

    DtoLayer created

---
+++ SignalR.DtoLayer/ReservationDto/CreateReservationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.ReservationDto
+{
+    public class CreateReservationDto
+    {
+     
+        public string ReservationName { get; set; }
+        public string ReservationPhone { get; set; }
+        public string ReservationEmail { get; set; }
+        public int ReservationPersonCount { get; set; }
+        public DateTime ReservationDate { get; set; }
+    }
+}
