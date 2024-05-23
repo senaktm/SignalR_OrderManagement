030370123cda17b00699e6acff6498212f1a02b6
senaktm <senaoktm@gmail.com>
2024-05-23 23:58:06 +0300

    Hubs for signalR created

---
+++ SignalR_API/Hubs/SignalRHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Concrete;
+
+namespace SignalR_API.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        Context context = new Context();
+        public async Task SendCategoryCount()
+        {
+            var value=context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount",value);
+        }
+    }
+}
