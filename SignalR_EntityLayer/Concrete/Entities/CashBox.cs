cac966ebf463fb4131728e96a9580a8d11bca559
senaktm <senaoktm@gmail.com>
2024-05-27 22:44:05 +0300

    new entity cashbox created

---
+++ SignalR_EntityLayer/Concrete/Entities/CashBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Concrete.Entities
+{
+    public class CashBox
+    {
+        public int CashBoxID { get; set; }
+        public decimal CashBoxTotalAmount { get; set; }
+    }
+}
